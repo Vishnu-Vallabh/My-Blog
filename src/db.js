23bedd55848c7efe01196e6d848032edb8751c3b
G. Vishnu Vallabh Rao <vishnug.be26@uceou.edu>
2025-03-23 00:48:28 +0530

    db.js

--- src/db.js
+++ src/db.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
     try {
-        await mongoose.connect("mongodb://127.0.0.1:27017/myblogDB", {
+        await mongoose.connect("mongodb string connection ", {
             useNewUrlParser: true,
             useUnifiedTopology: true,
         });
